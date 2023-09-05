ff13afbbceaa75714bd9cb88ac16a20c22574900
OleksandrGailiunas <alexgailiunas0011@gmail.com>
2023-09-05 14:14:09 +0300

    alert dialor with login layout has been added

--- lib/screens/basket_screen_directory/basket_screen.dart
+++ lib/screens/basket_screen_directory/basket_screen.dart
@@ -39,16 +39,16 @@ class BasketScreen extends StatelessWidget {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 20),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text('Ввійти',
-                        style: Theme.of(context).textTheme.titleLarge),
-                    style: OutlinedButton.styleFrom(
-                      foregroundColor: Colors.transparent,
-                      side: BorderSide(
-                          color: Theme.of(context).unselectedWidgetColor),
-                      maximumSize: const Size(300, 50),
-                      minimumSize: const Size(260, 50),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {
+                        _loginDialog(context);
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,),
+                      child: const Text('Ввійти',
+                          style: TextStyle(color:Colors.white,fontSize: 18)),
                     ),
                   ),
                 ),
@@ -59,4 +59,174 @@ class BasketScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+void _loginDialog(BuildContext context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: SingleChildScrollView(
+            child: AlertDialog(
+              insetPadding: const EdgeInsets.all(10),
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 110),
+                    child: Text('Вхід'),
+                  ),
+                  IconButton(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const Icon(Icons.close))
+                ],
+              ),
+              content: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: GestureDetector(
+                            child: Container(
+                                padding: const EdgeInsets.all(6),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                  color: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .focusColor!,
+                                )),
+                                height: 60,
+                                width: 60,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Image.asset('lib/images/apple.png'),
+                                ))),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: GestureDetector(
+                            child: Container(
+                                padding: const EdgeInsets.all(6),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                  color: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .focusColor!,
+                                )),
+                                height: 60,
+                                width: 60,
+                                child: Padding(padding: const EdgeInsets.all(5),child: Image.asset('lib/images/google.png')),),),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                            thickness: 0.5,
+                            color: Theme.of(context)
+                                .inputDecorationTheme
+                                .focusColor!),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 7, right: 7, bottom: 4),
+                        child: Text('або введи свій e-mail'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color:
+                              Theme.of(context).inputDecorationTheme.focusColor!,
+                        ),
+                      )
+                    ],
+                  ),
+                  TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Адреса e-mail',
+                        hintStyle:
+                            TextStyle(color: Theme.of(context).disabledColor),
+                      ),
+                      cursorColor: Colors.grey.shade400),
+                  TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Пароль',
+                        hintStyle:
+                            TextStyle(color: Theme.of(context).disabledColor),
+                        suffixIcon: Icon(Icons.remove_red_eye_outlined,
+                            color: Theme.of(context)
+                                .inputDecorationTheme
+                                .focusColor),
+                      ),
+                      cursorColor: Colors.grey.shade400),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,),
+                        child: Text('Вхід',
+                         style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                   Text(
+                    'Забули пароль?',
+                    style: TextStyle(
+                      color: Theme.of(context).disabledColor,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                   Center(
+                    child: Text.rich(
+                      TextSpan(
+                          text: 'Користуючись додатком ви погоджуєтесь з ',style: TextStyle(color: Theme.of(context).disabledColor,),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: 'Правилами магазину',
+                                style: TextStyle(
+                                  color: Theme.of(context).disabledColor,
+                                  decoration: TextDecoration.underline,
+                                ))
+                          ]),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Реєстрація',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).focusColor,
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
 }
--- lib/screens/home_screen_directory/customer_home_screen.dart
+++ lib/screens/home_screen_directory/customer_home_screen.dart
@@ -3,18 +3,23 @@ import 'package:flutter/material.dart';
 import '../../widgets/home_screen_widgets/brands_grid_widget.dart';
 import '../../widgets/home_screen_widgets/container_of_products_for_sex.dart';
 
-class CustomerHomeScreen extends StatelessWidget {
+class CustomerHomeScreen extends StatefulWidget {
   final String firstImage;
 
   const CustomerHomeScreen({super.key, required this.firstImage});
 
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 4, left: 14, right: 14),
       child: ListView(
         children: [
-          Image.asset(firstImage),
+          Image.asset(widget.firstImage),
           const SizedBox(
             height: 20,
           ),
